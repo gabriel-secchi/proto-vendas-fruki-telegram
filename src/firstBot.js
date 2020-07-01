135bea29cc52598d81d07277d97c138cc8412019
{seu_usuario} <{seu_usuario}@fruki.com.br>
2020-07-01 07:56:12 -0300

    Fazendo testes

--- src/firstBot.js
+++ src/firstBot.js
@@ -1,21 +1,39 @@
 const env       = require('../.env')
 const Telegraf  = require('telegraf')
+const Extra    = require('telegraf/extra')
 const Markup    = require('telegraf/markup')
+const { markup } = require('telegraf/extra')
 const bot       = new Telegraf(env.token)
 
+const botoes = Extra.markup(Markup.inlineKeyboard([
+    Markup.callbackButton('teste 123', 'aaa'),
+    Markup.callbackButton('qqqqq', '*qwe')
+]))
+
+var teste = null;
 
 bot.start( async ctx => {
-    const from = ctx.update.message.from 
+    console.log('aaaa')
+    /* const from = ctx.update.message.from 
     
     console.log(`${from.first_name} entrou!`)
-
+    teste = from.first_name;
     await ctx.reply(`Olá ${from.first_name}, preciso do número do seu telefone para continuarmos.`);
-    ctx.reply(
+    await ctx.reply("teste btn", botoes) */
+    
+    /* ctx.reply(
         'Poderia me enviar?',
         Markup.keyboard(
             [{text: "📲 Enviar meu número", request_contact: true}]
         ).resize().oneTime().extra()
-    )
+    ) */
+})
+
+bot.action('*qwe', async ctx => {
+    console.log('aqui 12345')
+    //console.log(ctx.update.callback_query.from.id)
+    //console.log( await ctx.getChatMember(ctx.update.callback_query.from.id) ) 
+    console.log(`ultimo chamada ${teste}`)
 })
 
 bot.on('text', ctx => {
